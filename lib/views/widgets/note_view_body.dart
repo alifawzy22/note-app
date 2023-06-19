85f52f83c24aeafe9ea57f49d7bd20f33d4b056b
Ali Fawzy <a.fawzy@digitrans.gov.eg>
2023-06-19 11:31:33 +0300

    add note view body and custom app bar

---
+++ lib/views/widgets/note_view_body.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'custom_appbar.dart';
+
+class NoteViewBody extends StatelessWidget {
+  const NoteViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 50, left: 24, right: 24),
+      child: Column(
+        children: const [
+          CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
